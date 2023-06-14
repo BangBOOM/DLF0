6530c0cdabefb24e2e77cb8725708543422c5e62
Arik <looplearngotoloop@gmail.com>
2023-06-14 23:34:08 +0800

    update

--- transformer/encoder_layer.jl
+++ transformer/encoder_layer.jl
@@ -1,14 +1,11 @@
-# I have a sentence: I am learning Julia. 
-# I will init a matrix with 4 rows and 1024 columns to represent the sentence.
 using Statistics
+using BenchmarkTools
 
-
-function my_rand(d1, d2)
-    rand(d1, d2).* 2 .- 1
+function my_rand(d...)
+    rand(Float32, d...) .* 2 .- 1
 end
 
 function normalize(arr)
-    
     (arr .- mean(arr)) / std(arr)
 end
 
@@ -16,73 +13,64 @@ function softmax(x)
     tmp = exp.(x)
     tmp ./ sum(tmp)
 end
+struct EncoderLayer
+    HEAD_COUNT::Int
+    TOKEN_COUNT::Int
+    TOKEN_DIM::Int
+    ATTEN_DIM::Int
 
-function positional_encoding(pos, dim, b)
-    pos_encoding = zeros(pos, dim)
-    @simd for p in 0:pos-1
-        for i in 0:div(dim, 2)-1
-            tmp = b ^ (2i/dim)
-            pos_encoding[p+1, 2i+1] = sin(p/tmp)
-            pos_encoding[p+1, 2i+2] = cos(p/tmp)
-        end
+    WQ::Array{Float32}
+    WK::Array{Float32}
+    WV::Array{Float32}
+
+    W::Array{Float32}
+    WF::Array{Float32}
+
+    EncoderLayer(HEAD_COUNT::Int, TOKEN_COUNT::Int, TOKEN_DIM::Int, ATTEN_DIM::Int) = new(
+        HEAD_COUNT,
+        TOKEN_COUNT,
+        TOKEN_DIM,
+        ATTEN_DIM,
+        my_rand(HEAD_COUNT, TOKEN_DIM, ATTEN_DIM),
+        my_rand(HEAD_COUNT, TOKEN_DIM, ATTEN_DIM),
+        my_rand(HEAD_COUNT, TOKEN_DIM, ATTEN_DIM),
+        my_rand(ATTEN_DIM * HEAD_COUNT, TOKEN_DIM),
+        my_rand(TOKEN_DIM, TOKEN_DIM)
+    )
+end
+
+Base.show(io::IO, layer::EncoderLayer) = print(io, "EncoderLayer(head_count=$(layer.HEAD_COUNT), token_count=$(layer.TOKEN_COUNT), token_dim=$(layer.TOKEN_DIM), atten_dim=$(layer.ATTEN_DIM))")
+
+
+
+function forward(layer::EncoderLayer, tokens::Array{Float32})
+    Z = zeros(layer.TOKEN_COUNT, layer.ATTEN_DIM * layer.HEAD_COUNT)
+    @simd for h in 1:layer.HEAD_COUNT
+        WQ = layer.WQ[h, :, :]
+        WK = layer.WK[h, :, :]
+        WV = layer.WV[h, :, :]
+        Q = tokens * WQ
+        K = tokens * WK
+        V = tokens * WV
+        attention_output = Q * transpose(K) ./ sqrt(layer.TOKEN_DIM) |> softmax
+        attention_output = attention_output * V
+        Z[:, ((h-1)*layer.ATTEN_DIM+1):h*layer.ATTEN_DIM] = attention_output
     end
-    pos_encoding
+    W = layer.W
+    multi_attention_output = Z * W
+
+    add_output = multi_attention_output + tokens
+    nomalized_output = add_output |> normalize
+
+    WF = layer.WF
+    ffn_output = nomalized_output * WF
+    add_output = ffn_output + nomalized_output
+    normalized_output = add_output |> normalize
+
+    return normalized_output
 end
 
+tokens = my_rand(1024, 4096)
+layer = EncoderLayer(16, 1024, 4096, 256)
 
-TOKEN_CNT = 4
-TOKEN_DIM = 1024
-ATTEN_DIM = 512
-
-tokens = my_rand(TOKEN_CNT, TOKEN_DIM)
-
-# first step: do positional encoding
-tokens += positional_encoding(TOKEN_CNT, TOKEN_DIM, 1_000)
-
-
-# enter Self-Attention
-# one head
-#=
-WQ = my_rand(TOKEN_DIM, ATTEN_DIM)
-WK = my_rand(TOKEN_DIM, ATTEN_DIM)
-WV = my_rand(TOKEN_DIM, ATTEN_DIM)
-
-Q = tokens * WQ
-K = tokens * WK
-V = tokens * WV
-
-attention_output = Q * transpose(K) / sqrt(TOKEN_DIM) |> softmax 
-attention_output = attention_output * V
-=#
-
-# multi head
-HEAD_CNT = 8
-
-Z = rand(TOKEN_CNT, ATTEN_DIM * HEAD_CNT)
-for h in 1:HEAD_CNT
-    WQ = my_rand(TOKEN_DIM, ATTEN_DIM)
-    WK = my_rand(TOKEN_DIM, ATTEN_DIM)
-    WV = my_rand(TOKEN_DIM, ATTEN_DIM)
-    Q = tokens * WQ
-    K = tokens * WK
-    V = tokens * WV
-    attention_output = Q * transpose(K) / sqrt(TOKEN_DIM) |> softmax 
-    attention_output = attention_output * V
-    Z[:, ((h-1)*ATTEN_DIM+1):h*ATTEN_DIM] = attention_output
-end
-
-W = my_rand(ATTEN_DIM * HEAD_CNT, TOKEN_DIM)
-multi_attention_output = Z * W
-
-# ADD & Normalization
-add_output = multi_attention_output + tokens
-normlized_output = add_output |> normalize
-
-WF = my_rand(TOKEN_DIM, TOKEN_DIM)
-ffn_output = normlized_output * WF
-
-add_output = ffn_output + tokens
-normlized_output = add_output |> normalize
-
-
-
+@benchmark forward($layer, $tokens)
